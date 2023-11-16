863934372c3c8ce492c7d3f474929111ebc3321b
Sheriff Gatsi <sheriffgatsi690@gmail.com>
2023-11-16 10:18:20 +0200

    Modified the Header.css to have a responsive navbar as well as adding a  site logo

--- src/Components/HeaderComponent.jsx
+++ src/Components/HeaderComponent.jsx
@@ -1,53 +1,53 @@
 import React, { Component } from "react";
 
 class HeaderComponent extends Component {
-    state = {clicked: false}
-    handleClick = ()=>{
-        this.setState({
-            clicked:!this.state.clicked
-        })
-    }
-    render(){
-    return (
-        <div className="header">
-            <div id="home">
-                <a href="index.html"><svg id="logo-14" width="73" height="49" viewBox="0 0 73 49" fill="none" xmlns=
-                "http://www.w3.org/2000/svg"> <path d="M46.8676 24C46.8676 36.4264 36.794 46.5 24.3676
-                46.5C11.9413 46.5 1.86765 36.4264 1.86765 24C1.86765 11.5736 11.9413 1.5 24.3676 1.5C36.794
-                1.5 46.8676 11.5736 46.8676 24Z" class="ccustom" fill="#68DBFF"></path> <path d="M71.1324
-                24C71.1324 36.4264 61.1574 46.5 48.8529 46.5C36.5484 46.5 26.5735 36.4264 26.5735 24C26.5735
-                11.5736 36.5484 1.5 48.8529 1.5C61.1574 1.5 71.1324 11.5736 71.1324 24Z" class="ccompli1" fill=
-                "#FF7917"></path> <path d="M36.6705 42.8416C42.8109 38.8239 46.8676 31.8858 46.8676 24C46.8676
-                16.1144 42.8109 9.17614 36.6705 5.15854C30.5904 9.17614 26.5735 16.1144 26.5735 24C26.5735 31.8858
-                30.5904 38.8239 36.6705 42.8416Z" class="ccompli2" fill="#5D2C02"></path> </svg></a>
-                <a className="site-title"  href="/">
-                    Home
-                </a>
-            </div>
-            <ul className="navLinks">
-                <CustomLink href="/employee"> Our Employees</CustomLink>
-                <CustomLink href="index.html">About UZ</CustomLink>
-                <CustomLink href="/requests" >Requests</CustomLink>
-                <CustomLink href="index.html">Entertainment</CustomLink>
-                <CustomLink href="index.html">Departments</CustomLink>
-            </ul>
-            <div id="mobile" onClick={this.handleClick}>
-                <i id="bars" className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
-            </div>
-        </div>
-                
-                
-    )
+  state = {clicked: false}
+  handleClick = ()=>{
+    this.setState({
+        clicked:!this.state.clicked
+    })
+}
+render(){
+return (
+    <div id="header">
+      <div id="home">
+        <a href="/"><svg id="logo-14" width="73" height="49" viewBox="0 0 73 49" fill="none" xmlns=
+        "http://www.w3.org/2000/svg"> <path d="M46.8676 24C46.8676 36.4264 36.794 46.5 24.3676
+        46.5C11.9413 46.5 1.86765 36.4264 1.86765 24C1.86765 11.5736 11.9413 1.5 24.3676 1.5C36.794
+        1.5 46.8676 11.5736 46.8676 24Z" class="ccustom" fill="#68DBFF"></path> <path d="M71.1324
+        24C71.1324 36.4264 61.1574 46.5 48.8529 46.5C36.5484 46.5 26.5735 36.4264 26.5735 24C26.5735
+        11.5736 36.5484 1.5 48.8529 1.5C61.1574 1.5 71.1324 11.5736 71.1324 24Z" class="ccompli1" fill=
+        "#FF7917"></path> <path d="M36.6705 42.8416C42.8109 38.8239 46.8676 31.8858 46.8676 24C46.8676
+        16.1144 42.8109 9.17614 36.6705 5.15854C30.5904 9.17614 26.5735 16.1144 26.5735 24C26.5735 31.8858
+        30.5904 38.8239 36.6705 42.8416Z" class="ccompli2" fill="#5D2C02"></path> </svg></a>
+        <a className="site-title"  href="/">
+          Home
+        </a>
+      </div>
+          <ul id="navLinks" className={this.state.clicked ? "#navLinks active" : "#navLinks"}>
+            <CustomLink href="/employee"> Employees</CustomLink>
+            <CustomLink href="/about">About UZ</CustomLink>
+            <CustomLink href="/requests" >Requests</CustomLink>
+            <CustomLink href="/entertainment">Entertainment</CustomLink>
+            <CustomLink href="/departments">Departments</CustomLink>
+                </ul>
+          <div id="mobile" onClick={this.handleClick}>
+            <i id="bars" className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
+    </div>
+            
+              
+  )
 };
 }
 
 function CustomLink({href, children, ...props}){
-    const path = window.location.pathname
-    return (
-        <li className= {path === href ? "active" : ""}>
-            <a href={href} {...props}>{children}</a>
-        </li>
-    )
+  const path = window.location.pathname
+  return (
+    <li className= {path === href ? "active" : ""}>
+        <a href={href} {...props}>{children}</a>
+    </li>
+  )
 }
 
 
