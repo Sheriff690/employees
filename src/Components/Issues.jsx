9784fc703dbb14b3e6aec9f4724cfae8e5305fcc
Sheriff Gatsi <sheriffgatsi690@gmail.com>
2023-11-17 16:31:19 +0200

    Commit on Friday 17 Nov

---
+++ src/Components/Issues.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './Issues.css'
+
+const Issues = ()=>{
+  return (
+    <div>
+      <div className="contRequests">
+        <h1>Have Queries, requests, contributions? Let's talk!</h1>
+        <p>Whenever you have some burning queries or questions, don't hesitate. Just get in touch!
+            We have a number of well qualified, caring and packed with so much petience personnel.
+        </p>
+        <div><a href="/requests">Make Request</a></div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Issues;
