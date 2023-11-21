9c53323a588beeb76801ca536137819598332610
Sheriff Gatsi <sheriffgatsi690@gmail.com>
2023-11-21 11:54:48 +0200

    After trying to configer the pckg.json.

--- src/Components/Requests.jsx
+++ src/Components/Requests.jsx
@@ -27,25 +27,31 @@ const Requests = () =>{
           <div className="card-body">
             <form  action="" onSubmit={handleSubmit}>
               <div className="form-group mb-2">
+              <label for="startingDate" class="form-label">From</label>
                 <input className="form-control" type="date"
+                id="startingDate"
                 value={startDate}
                 required
                 onChange={(e) =>setStartDate(e.target.value)}
                 />
               </div>
               <div className="form-group mb-2">
+              <label for="endDate" class="form-label">To</label>
                 <input className="form-control" type="date" 
+                id="endDate"
                 value={endDate}
                 required
                 onChange={(e)=>setEndDate(e.target.value)}
                 />
               </div>
               <div className="form-group mb-2">
+              <label for="reason" class="form-label">Reason</label>
                 <textarea className="form-control"   //Try to increase size of the textarea using bootstrap
+                id="reasonTextArea"
                 value={reason}
                 required
                 onChange={(e)=> setReason(e.target.value)}
-                type="text" placeholder="Breifly describe your situation here...">
+                type="text" placeholder="Breifly describe your situation, 'Why do you want a leave?', here...">
 
                 </textarea>
               </div>
